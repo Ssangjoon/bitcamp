eec894f87dff587ae877f5341bcd2d6562c53548
youcanrun <qkqh34k@gmail.com>
2021-12-29 19:58:39 +0900

    study

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -1,30 +1,28 @@
 package com.eomcs.mylist;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController 
+@RestController 
 public class ContactController {
 
-  Contact[] contacts = new Contact[5];
-  int size = 0;
+  // Contact 객체 목록을 저장할 메모리 준비
+  // => Object[] list = new Object[5];
+  // => int size = 0;
+  ArrayList contactList = new ArrayList();
 
   @RequestMapping("/contact/list")
   public Object list() {
-    String[] arr = new String[size]; 
-    for (int i = 0; i < size; i++) { 
-      arr[i] = contacts[i]; 
-    }
-    return arr; 
+    return ArrayList.toArray(contactList); 
   }
 
   @RequestMapping("/contact/add")
-  public Object add(String name, String email, String tel, String company) {
-    if (size == contacts.length) { // 배열이 꽉찼다면,
-      contacts = grow(); // 메서드 이름에서 해당 코드에 대한 설명을 짐작할 수 있다.
-    }
-    contacts[size++] = createCSV(name, email, tel, company);
-    return size;
+  public Object add(Contact contact) {
+    //    System.out.println(contact);
+    ArrayList.add(contactList, contact);
+    return contactList.size;
   }
+
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
@@ -32,19 +30,17 @@ public class ContactController {
     if (index == -1) {
       return "";
     }
-
-    return contacts[index];
+    return contactList.list[index];
   }
 
   @RequestMapping("/contact/update")
-  public Object update(String name, String email, String tel, String company) {
-    int index = indexOf(email);
+  public Object update(Contact contact) {
+    int index = indexOf(contact.email);
     if (index == -1) {
       return 0;
     }
 
-    contacts[index] = createCSV(name, email, tel, company);
-    return 1;
+    return ArrayList.set(contactList, index, contact) == null ? 0 : 1;
   }
 
   @RequestMapping("/contact/delete")
@@ -54,72 +50,18 @@ public class ContactController {
       return 0;
     }
 
-    remove(index);  // 메서드 이름으로 코드의 의미를 짐작할 수 있다. 이것이 메서드로 분리하는 이유이다.
+    ArrayList.remove(contactList, index);
     return 1;
   }
 
-  // 기능: 
-  // - 입력 받은 파라미터 값을 가지고 CSV 형식으로 문자열을 만들어 준다.
-  //
-  String createCSV(String name, String email, String tel, String company) {
-    return name + "," + email + "," + tel + "," + company;
-  }
-
-  // 기능:
-  // - 이메일로 연락처 정보를 찾는다.
-  // - 찾은 연락처의 배열 인덱스를 리턴한다.
-  //
   int indexOf(String email) {
-    for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) { 
+    for (int i = 0; i < contactList.size; i++) {
+      Contact contact =  (Contact) contactList.list[i];
+      if (contact.email.equals(email)) { 
         return i;
       }
     }
     return -1;
-  }
-
-  // 기능:
-  // - 배열에서 지정한 항목을 삭제한다.
-  //
-  String remove(int index) {
-    String old = contacts[index];
-    for (int i = index + 1; i < size; i++) {
-      contacts[i - 1] = contacts[i];
-    }
-    size--;
-    return old;
-  }
-
-  // 기능:
-  // - 배열의 크기를 늘린다.
-  // - 기존 배열의 값을 복사해온다.
-  //
-  String[] grow() {
-    String[] arr = new String[newLength()];
-    copy(contacts, arr);
-    return arr;
-  }
-
-  // 기능:
-  // - 주어진 배열에 대해 50% 증가시킨 새 배열의 길이를 알려준다.
-  //
-  int newLength() {
-    return contacts.length + (contacts.length >> 1);
-  }
-
-  // 기능: 
-  // - 배열을 복사한다.
-  // 
-  void copy(String[] source, String[] target) {
-    // 개발자가 잘못 사용할 것을 대비해서 다음 코드를 추가한다.
-    // 즉 target 배열이 source 배열 보다 작을 경우 target 배열 크기만큼만 복사한다.
-    int length = source.length;
-    if (target.length < source.length) {
-      length = target.length;
-    }
-    for (int i = 0; i < length; i++) {
-      target[i] = source[i];
-    }
   }
 }
 
