706bde9b60720baa59c42f99ed93c4fc549de7af
youcanrun <qkqh34k@gmail.com>
2022-01-17 17:13:51 +0900

    I/O stream - FIle class , FileOutputStream/FIleInputStream (byte stream class)

--- java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0730x.java
+++ java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0730x.java
@@ -2,9 +2,8 @@
 package com.eomcs.io.ex01;
 
 import java.io.File;
-import java.io.FileFilter;
 
-public class Exam0731 {
+public class Exam0730x {
 
   public static void main(String[] args) throws Exception {
 
@@ -15,32 +14,19 @@ public class Exam0731 {
     File dir = new File("bin/main");
     System.out.println(dir.getCanonicalPath());
 
-    printClasses(dir, "");
+    printClasses(dir);
   }
+  static boolean printClasses(File dir) {
+    class JavaClassFilter implements FileFilter {
 
-  static void printClasses(File dir, String packageName) {
-
-    File[] files = dir.listFiles(new FileFilter() {
-      @Override
-      public boolean accept(File pathname) {
-        return pathname.isDirectory() || (pathname.isFile() && pathname.getName().endsWith(".class"));
-      }
-    });
-
-    if (packageName.length() > 0) {
-      packageName += ".";
     }
+    File[] files = dir.listFiles();
 
-    for (File file : files) {
-      if (file.isDirectory()) {
-        printClasses(file, packageName + file.getName());
+    for(File f : files) {
+      if (f.isFile() && f.getName().endsWith(".class")){
+        System.out.println(f.getName());
+      } else if () {
 
-      } else {
-        // path가 파일이면 패키지 이름과 파일 이름을 합쳐 출력하고 리턴한다.
-        // => 단 파일 이름에서 .class 확장자 명은 제외한다.
-        // => 파일 명이 Hello.class 이고 패키지명이 aaa.bbb 라면
-        //    출력할 이름은 aaa.bbb.Hello 이다.
-        System.out.println(packageName + file.getName().replace(".class", ""));
       }
     }
   }
