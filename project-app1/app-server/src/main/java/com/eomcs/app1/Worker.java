45622b785757fcaf9e6e0760b4cea52cdba53bbb
youcanrun <qkqh34k@gmail.com>
2022-02-02 23:35:59 +0900

    .

--- project-app1/app-server/src/main/java/com/eomcs/app1/Worker.java
+++ project-app1/app-server/src/main/java/com/eomcs/app1/Worker.java
@@ -2,12 +2,14 @@ package com.eomcs.app1;
 
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.util.Scanner;
 
-public class Worker extends Thread{
+public class Worker extends Thread {
+
   Socket socket;
 
-  public Worker (Socket socket ) {
+  public Worker(Socket socket) {
     this.socket = socket;
   }
 
@@ -17,65 +19,73 @@ public class Worker extends Thread{
       Scanner in = new Scanner(socket.getInputStream());
       PrintStream out = new PrintStream(socket.getOutputStream());
 
-
+      // 1) HTTP 요청 데이터 읽기
       String requestLine = in.nextLine();
-      String[] values = requestLine.split("/");
+      System.out.println(requestLine);
 
-      while(true) {
+      // 나머지 데이터는 버린다.
+      while (true) {
         String str = in.nextLine();
-        if (str.length() ==0 ) {
+        if (str.length() == 0) {
           break;
         }
       }
-      String response = "Hello!";
 
-      out.println("HTTP/1.1 200 OK");
-      out.println("Content-Type: text/plain; charset=UTF-8");
-      out.printf("Content-Length: %d\n", response);
-      out.println();
-      out.println(response);
-      out.flush();
+      // 예) requestLine =  "GET /+/100/200 HTTP/1.1"
+      String requestUri = requestLine.split(" ")[1]; // 예) "/+/100/200"
+      String[] values = requestUri.split("/"); // 예) {"", "plus", "100", "200"}
 
+      if (values.length == 4) {
+        String op = URLDecoder.decode(values[1], "UTF-8"); // "%2b" -> "+", "-", "*", "%2f" -> "/"
+        int a = Integer.parseInt(values[2]); // "100"
+        int b = Integer.parseInt(values[3]); // "200"
+        System.out.printf("%s, %d, %d\n", op, a, b);
 
-      if (values.length != 3) {
-        out.println("계산식이 올바르지 않습니다.");
+        String response = null;
 
-      }else {
-        String op = values[0];
-        if(op.equals("%2f")) {
-          op = "/";
+        switch (op) {
+          case "+": 
+            response = String.format("강사: %d + %d = %d", a, b, (a + b));
+            break;
+          case "-": 
+            response = String.format("강사: %d - %d = %d", a, b, (a - b));
+            break;
+          case "*": 
+            response = String.format("강사: %d * %d = %d", a, b, (a * b));
+            break;  
+          case "/": 
+            response = String.format("강사: %d / %d = %d", a, b, (a / b));
+            break;
+          default: 
+            response = "강사: 지원하지 않는 연산자입니다.";
         }
-        int a = Integer.parseInt(values[1]);
-        int b = Integer.parseInt(values[2]);
-        int result = 0;
+        writeResponse(out, response);
 
-        switch(op) {
-          case "+" : 
-            result = a + b;
-            out.printf("상준 : %d %s %d = %d\n", a,op,b,result);
-            break;
-          case "-" : 
-            result = a - b;
-            out.printf("상준 : %d %s %d = %d\n", a,op,b,result);
-            break;
-          case "/" : 
-            result = a / b;
-            out.printf("상준 : %d %s %d = %d\n", a,op,b,result);
-            break;
-          case "*" : 
-            result = a * b;
-            out.printf("상준 : %d %s %d = %d\n", a,op,b,result);
-            break;
-          default:
-            out.println("상준 : ㅎ 계산이 하고 싶어?");
-        }
+      } else {
+        writeResponse(out, "요청 형식이 올바르지 않습니다.");
       }
 
       socket.close();
-      System.out.println("클라이언트 연결 종료");
+      System.out.println("클라이언트 연결 종료!");
 
     } catch (Exception e) {
       e.printStackTrace();
     }
-  } 
+  }
+
+  // HTTP 응답 데이터 보내기
+  private void writeResponse(PrintStream out, String messageBody) throws Exception {
+    out.println("HTTP/1.1 200 OK");
+    out.println("Content-Type: text/plain; charset=UTF-8");
+    out.println();
+    out.print(messageBody);
+    out.flush();
+  }
 }
+
+
+
+
+
+
+
