9d093eec61161959b3440af66e06a7d69d3e4377
youcanrun <qkqh34k@gmail.com>
2022-03-12 10:28:31 +0900

    second dbmodeling reiview

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,14 +1,55 @@
 package com.eomcs.mylist;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class App {
+
+  @Value("${spring.dataSource.driver-class-name}")
+  String driverClassName;
+  @Value("${spring.dataSource.url}")
+  String url;
+  @Value("${spring.dataSource.username}")
+  String userName;
+  @Value("${spring.dataSource.password}")
+  String passWord;
+
+  //스프링프레임워크 (IoC = 객체풀)에서 객체를 생성한 후 보관하도록 만드는 방법
+  @Bean 
+  // => 스프링부트를 시작 할 때 다음 메서드를 호출하게 만든다. 
+  // 이 메서드가 리턴한 값은 스프링 부트의 객체풀에 보관한다. 
+  public DataSource DataSource(
+      @Value("${spring.dataSource.driver-class-name}") String driverClassName,
+      String url,
+      String userName,
+      String passWord
+      ) {
+    System.out.println("createDataSource() 호출됨!!");
+    System.out.println(driverClassName);
+    System.out.println(url);
+    System.out.println(userName);
+    System.out.println(passWord);
+    try {
+      DriverManagerDataSource connectionPool = new DriverManagerDataSource();
+      connectionPool.setDriverClassName(driverClassName);
+      connectionPool.setUrl(url);
+      connectionPool.setUsername(userName);
+      connectionPool.setPassword(passWord);
+
+      return connectionPool;
+    } catch(Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
@@ -1,12 +1,15 @@
-package com.eomcs.mylist.dao;
+package com.eomcs.mylist.dao.mariadb;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import com.eomcs.mylist.dao.BoardDao;
+import com.eomcs.mylist.dao.DaoException;
 import com.eomcs.mylist.domain.Board;
 
 // @Repository
@@ -14,29 +17,32 @@ import com.eomcs.mylist.domain.Board;
 // - 또한 이 객체를 원하는 곳에 자동으로 주입한다.
 //
 @Repository  
-public class JdbcBoardDao implements BoardDao {
+public class BoardDaoImpl implements BoardDao {
 
-  public JdbcBoardDao() {
-    System.out.println("JdbcBoardDao 객체 생성!");
+  @Autowired // => 스프링 프레임워크에게 보관된 객체 중에서 다음 타입의 객체가 있다면 주입 해줄것을 지시하는 애노테이션 
+  DataSource dataSource;
+  public BoardDaoImpl() {
+    System.out.println("BoardDaoImpl 객체 생성!");
   }
 
   @Override
-  public int countAll() throws Exception {
-    try (Connection con = DriverManager.getConnection( //
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public int countAll() {
+    try (Connection con = dataSource.getConnection();
+        //DataSource 에서 얻은 커텍션 객체는 close 할때 연결을 끊는 것이 아니라 DataSource에 반납된다. 
         PreparedStatement stmt = con.prepareStatement( //
             "select count(*) from ml_board");
         ResultSet rs = stmt.executeQuery()) {
 
       rs.next();
       return rs.getInt(1);
+    } catch (Exception e) {
+      throw new DaoException(e);
     }
   }
 
   @Override
-  public List<Board> findAll() throws Exception {
-    try (Connection con = DriverManager.getConnection( 
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public List<Board> findAll() {
+    try (Connection con = dataSource.getConnection();
         PreparedStatement stmt = con.prepareStatement( 
             "select board_no,title,created_date,view_count from ml_board order by board_no desc");
         ResultSet rs = stmt.executeQuery()) {
@@ -51,13 +57,14 @@ public class JdbcBoardDao implements BoardDao {
         arr.add(board);
       }
       return arr;
+    }catch (Exception e) {
+      throw new DaoException(e);
     }
   }
 
   @Override
-  public int insert(Board board) throws Exception {
-    try (Connection con = DriverManager.getConnection( //
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public int insert(Board board){
+    try (Connection con = dataSource.getConnection();
         PreparedStatement stmt =
             con.prepareStatement("insert into ml_board(title,content) values(?,?)");) {
 
@@ -65,13 +72,14 @@ public class JdbcBoardDao implements BoardDao {
       stmt.setString(2, board.getContent());
 
       return stmt.executeUpdate();
+    }catch (Exception e) {
+      throw new DaoException(e);
     }
   }
 
   @Override
-  public Board findByNo(int no) throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public Board findByNo(int no) {
+    try (Connection con = dataSource.getConnection();
         PreparedStatement stmt = con.prepareStatement(
             "select board_no,title,content,created_date,view_count from ml_board where board_no=?")) {
 
@@ -89,13 +97,15 @@ public class JdbcBoardDao implements BoardDao {
         board.setViewCount(rs.getInt("view_count"));
         return board;
       }
+    }catch (Exception e) {
+      throw new DaoException(e);
     }
+
   }
 
   @Override
-  public int update(Board board) throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public int update(Board board) {
+    try (Connection con = dataSource.getConnection();
         PreparedStatement stmt = con.prepareStatement(
             "update ml_board set title=?, content=? where board_no=?")) {
 
@@ -104,30 +114,34 @@ public class JdbcBoardDao implements BoardDao {
       stmt.setInt(3, board.getNo());
 
       return stmt.executeUpdate();
+    }catch (Exception e) {
+      throw new DaoException(e);
     }
   }
 
   @Override
-  public int delete(int no) throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public int delete(int no) {
+    try (Connection con = dataSource.getConnection();
         PreparedStatement stmt = con.prepareStatement(
             "delete from ml_board where board_no=?")) {
 
       stmt.setInt(1, no);
       return stmt.executeUpdate();
+    }catch (Exception e) {
+      throw new DaoException(e);
     }
   }
 
   @Override
-  public int increaseViewCount(int no) throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+  public int increaseViewCount(int no) {
+    try (Connection con = dataSource.getConnection();
         PreparedStatement stmt = con.prepareStatement(
             "update ml_board set view_count=view_count + 1 where board_no=?")) {
 
       stmt.setInt(1, no);
       return stmt.executeUpdate();
+    }catch (Exception e) {
+      throw new DaoException(e);
     }
   }
 
