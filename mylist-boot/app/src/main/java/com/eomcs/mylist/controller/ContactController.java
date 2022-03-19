ed0cef503d10628c8e1d0a2458b9c0645f9f5da5
youcanrun <qkqh34k@gmail.com>
2022-03-19 10:36:40 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -20,14 +20,17 @@ public class ContactController {
     for (Contact contact : contacts) {
       contact.setTels(contactDao.findTelByContactNo(contact.getNo()));
     }
-    return contacts; 
+    return contacts;
   }
 
   @RequestMapping("/contact/add")
   public Object add(Contact contact, String[] tel) throws Exception {
     contactDao.insert(contact);
     for (int i = 0; i < tel.length; i++) {
-      String[] value = tel[i].split(",");
+      String[] value = tel[i].split("_");
+      if (value[1].length() == 0) {
+        continue;
+      }
       contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
     }
     return 1;
@@ -36,16 +39,32 @@ public class ContactController {
   @RequestMapping("/contact/get")
   public Object get(int no) {
     Contact contact = contactDao.findByNo(no);
-    return contact != null ? contact : "";
+    if (contact == null) {
+      return "";
+    }
+    contact.setTels(contactDao.findTelByContactNo(no));
+    return contact;
   }
 
   @RequestMapping("/contact/update")
-  public Object update(Contact contact) throws Exception {
-    return contactDao.update(contact);
+  public Object update(Contact contact, String[] tel) throws Exception {
+    int count = contactDao.update(contact);
+    if (count > 0) {
+      contactDao.deleteTelByContactNo(contact.getNo());
+      for (int i = 0; i < tel.length; i++) {
+        String[] value = tel[i].split("_");
+        if (value[1].length() == 0) {
+          continue;
+        }
+        contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
+      }
+    }
+    return count;
   }
 
   @RequestMapping("/contact/delete")
   public Object delete(int no) throws Exception {
+    contactDao.deleteTelByContactNo(no);
     return contactDao.delete(no);
   }
 
