45e7cef9885fb001b26908cfe3c750d82b562fda
eomjinyoung <jinyoung.eom@gmail.com>
2022-04-12 11:35:00 +0900

    .

--- project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardListServlet.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/board/BoardListServlet.java
@@ -2,22 +2,46 @@ package com.eomcs.mylist.web.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.eomcs.mylist.domain.Board;
+import com.eomcs.mylist.service.BoardService;
 
 @SuppressWarnings("serial")
 @WebServlet("/board/list") 
 public class BoardListServlet extends HttpServlet {
 
+  BoardService boardService;
+
+  @Override
+  public void init() throws ServletException {
+    // BoardService 객체를 웹애플리케이션 보관소에서 꺼낸다.
+    ServletContext 웹애플리케이션보관소 = this.getServletContext();
+    boardService = (BoardService) 웹애플리케이션보관소.getAttribute("boardService");
+  }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+
+    List<Board> boards = boardService.list();
+    for (Board board : boards) {
+      System.out.println(board.getTitle());
+    }
 
     resp.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = resp.getWriter();
     out.println("게시글 목록");
   }
 }
+
+
+
+
+
+
